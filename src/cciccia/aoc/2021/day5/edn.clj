fbd359d58f7cfc48cd99d3231ed13db57d98818b
Charlie Ciccia <charlie.ciccia@singlewire.com>
2021-12-05 09:43:47 -0600

    merge solutions

--- src/cciccia/aoc/2021/day5/edn.clj
+++ src/cciccia/aoc/2021/day5/edn.clj
@@ -2,42 +2,16 @@
   (:require [cciccia.aoc.utils :as utils]))
 
 (defn plot-all-points
-  [input]
-  (reduce
-    (fn [p [[x1 y1] [x2 y2]]]
-      (if (or (= x1 x2) (= y1 y2))
-        (let [points (for [x (if (> x2 x1)
-                               (range x1 (inc x2) 1)
-                               (range x2 (inc x1) 1))
-                           y (if (> y2 y1)
-                               (range y1 (inc y2) 1)
-                               (range y2 (inc y1) 1))]
-                       [x y])]
-          (reduce
-            (fn [q c]
-              (update q c #(inc (or % 0))))
-            p
-            points))
-        p))
-    {}
-    input))
-
-(defn part1
-  [input]
-  (let [graph (plot-all-points input)]
-    (count (filter (fn [[_coord val]]
-                     (> val 1))
-                   graph))))
-
-(defn plot-all-points-2
-  [input]
+  [input diagonals?]
   (reduce
     (fn [p [[x1 y1] [x2 y2]]]
       (let [sx (cond (> x2 x1) 1 (< x2 x1) -1 :else 0)
             sy (cond (> y2 y1) 1 (< y2 y1) -1 :else 0)
-            points (for [i (range (inc (max (Math/abs ^long (- x1 x2))
-                                            (Math/abs ^long (- y1 y2)))))]
-                     [(+ x1 (* sx i)) (+ y1 (* sy i))])]
+            points (if (or diagonals? (zero? sx) (zero? sy))
+                     (for [i (range (inc (max (Math/abs ^long (- x1 x2))
+                                              (Math/abs ^long (- y1 y2)))))]
+                       [(+ x1 (* sx i)) (+ y1 (* sy i))])
+                     [])]
         (reduce
           (fn [q c]
             (update q c #(inc (or % 0))))
@@ -46,14 +20,14 @@
     {}
     input))
 
-(defn part2
-  [input]
-  (let [graph (plot-all-points-2 input)]
+(defn part1
+  [input diagonals?]
+  (let [graph (plot-all-points input diagonals?)]
     (count (filter (fn [[_coord val]]
                      (> val 1))
                    graph))))
 
 (comment
-  (part1 (utils/load-edn-input "2021/day5.edn"))
-  (part2 (utils/load-edn-input "2021/day5.edn")))
+  (part1 (utils/load-edn-input "2021/day5.edn") false)
+  (part1 (utils/load-edn-input "2021/day5.edn") true))
 
