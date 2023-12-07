7d229fffdde527572af2753a81095afb415383eb
Charlie Ciccia <cciccia@gmail.com>
2023-12-07 12:48:08 -0600

    cool refactor

--- src/cciccia/aoc/y2023/day7.clj
+++ src/cciccia/aoc/y2023/day7.clj
@@ -19,17 +19,24 @@
    "1" 1
    "0" 0})
 
-(defn eval-with-jokers
-  [f h]
-  (some
-    f
-    (or (apply combo/cartesian-product (map
-                                         (fn [i]
-                                           (if (= \0 (get h i))
-                                             (disj (set h) \0)
-                                             [(get h i)]))
-                                         (range 5)))
-        [["11111"]])))
+(def hand-values
+  {[5] 6
+   [4 1] 5
+   [3 2] 4
+   [3 1 1] 3
+   [2 2 1] 2
+   [2 1 1 1] 1
+   [1 1 1 1 1] 0})
+
+(defn expand-for-jokers
+  [h]
+  (or (apply combo/cartesian-product (map
+                                       (fn [i]
+                                         (if (= \0 (get h i))
+                                           (disj (set h) \0)
+                                           [(get h i)]))
+                                       (range 5)))
+      ['(\1 \1 \1 \1 \1)]))
 
 
 (defn tiebreaker-value
@@ -38,70 +45,13 @@
                           (* (int (Math/pow 15 i)) (card-values (str v))))
                         (reverse h))))
 
-(defn five-of-a-kind?
-  [h]
-  (apply = h))
-
-(defn four-of-a-kind?
-  [h]
-  (some (fn [s]
-          (apply = s))
-        (combo/combinations h 4)))
-
-(defn full-house?
-  [h]
-  (some
-    (fn [[s1 s2]]
-      (and (< 1 (count s1) 4)
-           (apply = s1)
-           (apply = s2)))
-    (combo/partitions h :min 2 :max 2)))
-
-(defn three-of-a-kind?
-  [h]
-  (some (fn [s]
-          (apply = s))
-        (combo/combinations h 3)))
-
-(defn two-pair?
-  [h]
-  (some
-    (fn [[s1 s2]]
-      (and (< 1 (count s1) 4)
-           (< (count (set s1)) (count s1))
-           (< (count (set s2)) (count s2))))
-    (combo/partitions h :min 2 :max 2)))
-
-(defn pair?
-  [h]
-  (some (fn [s]
-          (apply = s))
-        (combo/combinations h 2)))
-
 (defn hand-value
   [h]
   (+ (* (int (Math/pow 15 5))
-        (cond
-          (eval-with-jokers five-of-a-kind? h)
-          6
-
-          (eval-with-jokers four-of-a-kind? h)
-          5
-
-          (eval-with-jokers full-house? h)
-          4
-
-          (eval-with-jokers three-of-a-kind? h)
-          3
-
-          (eval-with-jokers two-pair? h)
-          2
-
-          (eval-with-jokers pair? h)
-          1
-
-          :else
-          0))
+        (apply max
+               (map
+                 #(hand-values (sort > (vals (frequencies %))))
+                 (expand-for-jokers h))))
      (tiebreaker-value h)))
 
 (defn part1
